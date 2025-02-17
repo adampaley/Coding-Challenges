48843e97fe3ea2405bbe4cacb1e9eb4ccda2b4b7
Adam Paley <apaley95@gmail.com>
2025-02-16 22:00:50 -0500

    completed coding challenge

---
+++ leet-code/0XXX/00XX/rotate-image/app.js
@@ -0,0 +1,49 @@
+/* https://leetcode.com/problems/rotate-image/description/
+48. Rotate Image
+
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+Example 2:
+
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+*/
+
+const rotate = (matrix) => {
+    let [left, top, right, bottom] = [0, 0, matrix.length - 1, matrix.length - 1]
+    let shift = 0
+
+    while (top < bottom) {
+        while (left + shift < right) {
+            let [newBottomRight, newBottomLeft, newTopLeft, newTopRight] = [matrix[top + shift][right], matrix[bottom][right - shift], matrix[bottom - shift][left], matrix[top][left + shift]]
+            
+            matrix[top + shift][right] = newTopRight
+            matrix[bottom][right - shift] = newBottomRight
+            matrix[bottom - shift][left] = newBottomLeft
+            matrix[top][left + shift] = newTopLeft
+            shift++
+        }
+        top++
+        bottom--
+        left++
+        right--
+        shift = 0
+    }
+};
